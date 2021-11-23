3d3adb0b7c1f458a3e8f25035fdd6237bf2fc50a
tomas lanterna <tomas.lanterna@hotmail.com>
2021-11-23 13:50:56 -0300

    Desafio clase 9 navegacion

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {NavegationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator,createMate } from '@react-navigation/native-stack';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import ItemListContainer from '../components/ItemListContainer';
+import HomeScreen from '../screens/HomeScreen';
 
 const Stack=createNativeStackNavigator();
 
 
-const ShopNavigator = () => {
-    <NavegationContainer>
-        <Stack.Navigator initialRouteName="HomeScreen">
+const ShopNavigator = () => (
+   <NavegationContainer>
+        <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Products" component={ItemListContainer}/>
         </Stack.Navigator>
     </NavegationContainer>
-}
+);
 
-export default ShopNavigator
+
+export default ShopNavigator;
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { View,Button} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation,route}) => {
     return (
-        <div>
-            
-        </div>
+        <View>
+            <Button onPress={()=>{navigation.navigate('Products')}} title='Ver Productos'/>
+        </View>
     )
 }
 
